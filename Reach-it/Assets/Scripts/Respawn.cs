0aa6123cf89918a135ccfc6ec4a67199163e194f
maytham-onaissi <maytham_onaissi@hotmail.com>
2021-01-07 19:53:41 +0300

    Create Respawn.cs

---
+++ Reach-it/Assets/Scripts/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
