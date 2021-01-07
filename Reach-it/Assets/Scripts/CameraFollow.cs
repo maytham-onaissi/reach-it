543ff5f3dc52897d63586a9ad4b8af0422404b8e
maytham-onaissi <maytham_onaissi@hotmail.com>
2021-01-07 19:53:17 +0300

    Create CameraFollow.cs

---
+++ Reach-it/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+ 
+
+    [SerializeField] GameObject player;
+
+    
+    
+    Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
