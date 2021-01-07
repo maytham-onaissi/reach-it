f26a924ca2aa44aec2f7620577314607c8509f1a
maytham-onaissi <maytham_onaissi@hotmail.com>
2021-01-07 19:53:46 +0300

    Create Shrink.cs

---
+++ Reach-it/Assets/Scripts/Shrink.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrink : MonoBehaviour
+{
+    [SerializeField] float duration;
+    [SerializeField] float shrinker;
+    [SerializeField] GameObject pickUpEffect;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine( Pickup(other));
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        Vector3 originalSize = player.transform.localScale;
+
+        player.transform.localScale *= shrinker;
+
+        GetComponent<Collider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        
+
+        yield return new WaitForSeconds(duration);
+
+        player.transform.localScale = originalSize;
+
+        GetComponent<Collider>().enabled = true;
+        GetComponent<MeshRenderer>().enabled = true;
+
+    }
+}
